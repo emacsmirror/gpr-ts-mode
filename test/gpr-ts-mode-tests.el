45d18585b9585e567d47a10697e466605c94a7c8
Troy Brown <brownts@troybrown.dev>
2024-04-02 17:45:43 -0400

    Cleanup tests.

--- test/gpr-ts-mode-tests.el
+++ test/gpr-ts-mode-tests.el
@@ -1,6 +1,6 @@
 ;;; gpr-ts-mode-tests.el --- Tests for Tree-sitter-based GNAT Project mode -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023 Troy Brown
+;; Copyright (C) 2023-2024 Troy Brown
 
 ;; This file is not part of GNU Emacs.
 
@@ -21,54 +21,89 @@
 
 (require 'ert)
 (require 'ert-x)
+(require 'gpr-ts-mode)
+(require 'treesit)
 
-(ert-deftest gpr-ts-mode-test-indentation ()
-  (ert-test-erts-file (ert-resource-file "indent.erts")))
+(defun default-transform (&optional expect-error)
+  "Default transform function for test.
 
-(ert-deftest gpr-ts-mode-test-indentation-with_declaration ()
-  (ert-test-erts-file (ert-resource-file "indent-with_declaration.erts")))
+If EXPECT-ERROR is non-nil, then check for an error in the parse tree,
+otherwise check that there is no error in the parse tree."
+  (gpr-ts-mode)
+  (if expect-error
+      (should (treesit-search-subtree
+               (treesit-buffer-root-node) "ERROR"))
+    (should (not (treesit-search-subtree
+                  (treesit-buffer-root-node) "ERROR")))))
 
-;; NOTE:
-;;
-;; These test cases are broken out into separate files as there appears
-;; to be bugs in the built-in ERTS test harness that have not yet been
-;; addressed.  This is causing test cases to use the wrong "code"
-;; segment when run interactively.
-;;
-;; See the following for details:
-;;
-;; - https://debbugs.gnu.org/cgi/bugreport.cgi?bug=62471
-;;
+(defun filling-transform ()
+  "Filling transform function for test."
+  (default-transform)
+  (fill-paragraph))
 
-(ert-deftest gpr-ts-mode-test-indentation-case_construction ()
-  (ert-test-erts-file (ert-resource-file "indent-case_construction.erts")))
+(defun indent-transform (&optional setup expect-error)
+  "Indentation transform function for test.
 
-(ert-deftest gpr-ts-mode-test-indentation-case_construction-nl ()
-  (ert-test-erts-file (ert-resource-file "indent-case_construction-nl.erts")))
+SETUP can be used to perform custom initialization.  If EXPECT-ERROR is
+non-nil, then check for an error in the parse tree, otherwise check that
+there is no error in the parse tree."
+  (default-transform expect-error)
+  (setq-local indent-tabs-mode nil)
+  (when setup
+    (funcall setup))
+  (goto-char (point-min))
+  (cl-flet ((line-length () (- (line-end-position)
+                               (line-beginning-position))))
+    (while (not (eobp))
+      (if (> (line-length) 0)
+          (if (= (following-char) ?\s)
+              (while (and (> (line-length) 0)
+                          (= (following-char) ?\s))
+                (delete-char 1))
+            (insert-char ?\s)))
+      (forward-line 1)
+      (beginning-of-line)))
+  (indent-region (point-min) (point-max)))
 
-(ert-deftest gpr-ts-mode-test-indentation-typed_string_declaration ()
-  (ert-test-erts-file (ert-resource-file "indent-typed_string_declaration.erts")))
+(defun navigation-transform (binding &optional arg)
+  "Navigation transform function for test.
 
-(ert-deftest gpr-ts-mode-test-indentation-typed_string_declaration-nl ()
-  (ert-test-erts-file (ert-resource-file "indent-typed_string_declaration-nl.erts")))
+Use BINDING to navigate with optional prefix ARG."
+  (default-transform)
+  (let ((current-prefix-arg arg))
+    (call-interactively (key-binding (kbd binding)))))
 
-(ert-deftest gpr-ts-mode-test-indentation-expression ()
-  (ert-test-erts-file (ert-resource-file "indent-expression.erts")))
+(defun newline-transform (&optional expect-error)
+  "Newline transform function for test.
 
-(ert-deftest gpr-ts-mode-test-indentation-paren_alignment ()
-  (ert-test-erts-file (ert-resource-file "indent-paren_alignment.erts")))
+If EXPECT-ERROR is non-nil, then check for an error in the parse tree,
+otherwise check that there is no error in the parse tree."
+  (default-transform expect-error)
+  (setq-local indent-tabs-mode nil)
+  (call-interactively #'newline))
 
-(ert-deftest gpr-ts-mode-test-indentation-comment ()
-  (ert-test-erts-file (ert-resource-file "indent-comment.erts")))
-
-(ert-deftest gpr-ts-mode-test-indentation-variable_declaration ()
-  (ert-test-erts-file (ert-resource-file "indent-variable_declaration.erts")))
-
-(ert-deftest gpr-ts-mode-test-filling ()
-  (ert-test-erts-file (ert-resource-file "filling.erts")))
-
-(ert-deftest gpr-ts-mode-test-navigation ()
-  (ert-test-erts-file (ert-resource-file "navigation.erts")))
+(dolist (file
+         (directory-files
+          (expand-file-name "resources"
+                            (file-name-directory
+                             (cond (load-in-progress load-file-name)
+                                   ((bound-and-true-p byte-compile-current-file)
+                                    byte-compile-current-file)
+                                   (t (buffer-file-name)))))
+          nil
+          (rx bos
+              (or (not ".")
+                  (seq "." (not "."))
+                  (seq ".." (+ anychar)))
+              )))
+  (let* ((file-noext (file-name-sans-extension file))
+         (file-path (ert-resource-file file))
+         (transform (cond ((string-suffix-p "-nl" file-noext) #'newline-transform)
+                          ((string-prefix-p "filling" file-noext) #'filling-transform)
+                          ((string-prefix-p "indent" file-noext) #'indent-transform)
+                          (t #'default-transform))))
+    (eval `(ert-deftest ,(intern (concat "gpr-ts-mode-test-" file-noext)) ()
+             (ert-test-erts-file ,file-path #',transform)))))
 
 (provide 'gpr-ts-mode-tests)
 
