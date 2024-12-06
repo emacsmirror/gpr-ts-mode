1d0609aa83d26596e4cd11559d658453ced018b7
Troy Brown <brownts@troybrown.dev>
2024-12-06 08:52:22 -0500

    Bump version.

--- gpr-ts-mode.el
+++ gpr-ts-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Troy Brown <brownts@troybrown.dev>
 ;; Created: February 2023
-;; Version: 0.6.7
+;; Version: 0.7.0
 ;; Keywords: gpr gnat ada languages tree-sitter
 ;; URL: https://github.com/brownts/gpr-ts-mode
 ;; Package-Requires: ((emacs "29.1"))
